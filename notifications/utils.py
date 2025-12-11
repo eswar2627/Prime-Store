9f07e54b3eb6dfb896e5af9d8cf5fb260cf437de
MAGULURI ESWAR <eswarmaguluri02@gmail.com>
2025-12-12 01:46:25 +0530

    Add files via upload

---
+++ notifications/utils.py
@@ -0,0 +1,11 @@
+from firebase_admin import messaging
+def send_fcm_notification(token, title, body):
+    message = messaging.Message(
+        notification=messaging.Notification(
+            title=title,
+            body=body
+        ),
+        token=token,
+    )
+    response = messaging.send(message)
+    return response
